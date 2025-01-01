146e00fa22a8decd50901f8c78f5e37d3212d1af
Tugssaikhan Bolorsaikhan <tgsuuhahah@gmail.com>
2025-01-01 21:16:44 +0800

    finished

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Fruit : MonoBehaviour
 {
     public GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -20,13 +20,12 @@ public class Fruit : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("fruit picked up");
             FindObjectOfType<SpawnFruits>().OnFruitPickedUp();
             FindObjectOfType<GameManager>().AddScore(10);
             Destroy(gameObject);
         }
-        else if (other.CompareTag("Finish")) {
-            Debug.Log("fruit missed");
+        else if (other.CompareTag("Finish"))
+        {
             FindObjectOfType<SpawnFruits>().OnFruitMissed();
             FindObjectOfType<GameManager>().FruitMissed();
             Destroy(gameObject);
